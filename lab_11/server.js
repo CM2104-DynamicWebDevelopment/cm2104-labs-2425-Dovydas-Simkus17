4f3bd9c55e23b065ca0d2a8ec2206c971e428a18
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-01-30 10:55:13 +0000

    Added add route

--- lab_11/server.js
+++ lab_11/server.js
@@ -15,5 +15,9 @@ app.get('/joke',function (req, res) {
  res.end(randomJocke);
  
 });
-
+app.get('/add', function(req,res){
+    var x = req.query.x;
+    var y = req.query.y;
+    res.send("X + Y="+(x+y));
+});
 app.listen(8080);
