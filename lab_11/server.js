bbc3e050ac3b3a075702d89c9a8a2a07bd91b65b
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-01-30 10:50:13 +0000

    Added joke route

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var knockknock = require('knock-knock-jokes');
 
 app.get('/', function(req,res){
     res.send("Hello world! by express");
@@ -7,4 +9,11 @@ app.get('/', function(req,res){
 app.get('/test', function(req,res){
     res.send("this is route 2");
 });
+app.get('/joke',function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ var randomJocke = knockknock();
+ res.end(randomJocke);
+ 
+});
+
 app.listen(8080);
