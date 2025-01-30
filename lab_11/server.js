daa5b94603e63123ac5df1c430a8311c437d4f17
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-01-30 11:28:00 +0000

    Dynamic Response to Form

--- lab_11/server.js
+++ lab_11/server.js
@@ -40,7 +40,8 @@ app.get('/calc', function(req,res){
     }
 });
 app.get('/getform', function(req,res){
-    
-    res.send("something")
+    var name = req.query.name;
+    var quest = req.query.quest;
+    res.send("Hi " +name+ "! I am sure you will " +quest+ "!");
 });
 app.listen(8080);
