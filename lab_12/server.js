b46fabd7489a956047fc3a1f7fb5444bbf833619
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-02-10 12:29:57 +0000

    Syntax button

--- lab_12/server.js
+++ lab_12/server.js
@@ -65,7 +65,8 @@ async function getTracks(searchterm,res){
                 "<div>" +
                     "<h2>"+track.name+"</h2>"+
                     "<h4>"+track.artists[0].name+"</h4>"+
-                    "<button onlick='window.location.href="+siteName+" + /searchTopTracks?searchterm="+track.artists[0].id+"'>Top Tracks!</button>"+
+                    "<button onclick='alert('Hell yeah')'>Does this work?</button>"+
+                    "<button onclick='window.location.href="+siteName+" + /searchTopTracks?searchterm="+track.artists[0].id+"'>Top Tracks!</button>"+
                     "<img src='"+track.album.images[0].url+"'>"+
                     "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
                 "</div>";
