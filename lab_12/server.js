4a003529ec095164a933598d606d72e9a383d08a
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-02-10 12:22:20 +0000

    checking for a syntax error

--- lab_12/server.js
+++ lab_12/server.js
@@ -65,7 +65,7 @@ async function getTracks(searchterm,res){
                 "<div>" +
                     "<h2>"+track.name+"</h2>"+
                     "<h4>"+track.artists[0].name+"</h4>"+
-                    "<button onlick='window.location.href='"+siteName+" + /searchTopTracks?searchterm="+track.artists[0].id+"''>Top Tracks!</button>"+
+                    "<button onlick='window.location.href="+siteName+" + /searchTopTracks?searchterm="+track.artists[0].id+"'>Top Tracks!</button>"+
                     "<img src='"+track.album.images[0].url+"'>"+
                     "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
                 "</div>";
