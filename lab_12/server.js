5b6a745be4dbe3c2bae201006b838e8497a885f0
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-02-10 12:05:32 +0000

    Moving things about

--- lab_12/server.js
+++ lab_12/server.js
@@ -26,6 +26,26 @@ async function getTopTrack(artist, res) {
     spotifyApi.getArtistTopTracks(artist, 'GB')
         .then(function(data){
             console.log(data.body);
+            var tracks = data.body.tracks.items
+            //Setting up an empty string to act as the response
+            var HTMLResponse = "";
+            var siteName = "primelucas-gateperson-8080.codio.io";
+            for(var i=0; i<tracks.length;i++){
+                var track = tracks[i];
+                console.log(track.name);
+
+                HTMLResponse = HTMLResponse +
+                "<div>" +
+                    "<h2>"+track.name+"</h2>"+
+                    "<h4>"+track.artists[0].name+"</h4>"+
+                    //"<button onlick='location.href='"+siteName+"/searchTopTracks?searchterm="+track.artists[0].id+"''>Top Tracks!</button>"+
+                    "<img src='"+track.album.images[0].url+"'>"+
+                    "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
+                "</div>";
+                //console.log(HTMLResponse);
+                
+            }
+            res.send(HTMLResponse);
         }, function(err){
             console.log('Something went wrong!',err);
         })
@@ -45,7 +65,7 @@ async function getTracks(searchterm,res){
                 "<div>" +
                     "<h2>"+track.name+"</h2>"+
                     "<h4>"+track.artists[0].name+"</h4>"+
-                    "<button onlick='location.href='"+siteName+"/searchTopTracks?searchterm="+track.artists[0].id+"''>Top Tracks!</button>"+
+                    "<button onlick='window.location.href='"+siteName+" + /searchTopTracks?searchterm="+track.artists[0].id+"''>Top Tracks!</button>"+
                     "<img src='"+track.album.images[0].url+"'>"+
                     "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
                 "</div>";
