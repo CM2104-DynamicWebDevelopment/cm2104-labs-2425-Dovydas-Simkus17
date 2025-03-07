c0dfa037114252a5fb4b189d2acd7859b1a069a6
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-03-07 20:21:50 +0000

    Will Work now

--- lab_15/server.js
+++ lab_15/server.js
@@ -66,8 +66,8 @@ app.get('/', function(req, res) {
       if (err) throw err;
       //the result of the query is sent to the users page as the "users" array
       res.render('pages/users', {
-        users: userResult,
-        usename: usersResult
+        users: usersResult,
+        usename: userResult
       })
 
     });
