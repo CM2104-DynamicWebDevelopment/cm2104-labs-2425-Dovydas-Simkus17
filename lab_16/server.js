090586422f5bc6aa5a5ec1d3d5bfb19d4422408a
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-03-13 10:23:57 +0000

    NO IDEA

--- lab_16/server.js
+++ lab_16/server.js
@@ -16,12 +16,12 @@ io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on('disconnect', function () {
-    console.log('user disconnected');
+        console.log('user disconnected');
     });
 
     socket.on('chat message', function(msg){
-        console.log('message: ' +msg);
-    })
+        console.log('message: ' + msg);
+    });
 });
 
 http.listen(8080, function(){
