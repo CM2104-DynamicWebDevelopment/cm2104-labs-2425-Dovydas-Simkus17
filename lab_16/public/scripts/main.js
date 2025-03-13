4e9e3bcb1cf878607582198d3cd2f3f1a0901937
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-03-13 10:25:22 +0000

    Form bug fixed

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -1,5 +1,5 @@
 var socket = io();
-('#form').submit(function () {
+$('#form').submit(function () {
     var message = $('#input').val();
     if (message) {
         socket.emit('chat message', message);
